b4c943906a0792f53d0e78d2aaee8004068a4b9d
Abhishek Yadav <67749693+abhishekY495@users.noreply.github.com>
2023-03-22 19:50:50 +0530

    Auth details

--- crowdin.rb
+++ crowdin.rb
@@ -1,5 +1,6 @@
 incall teraType || inreturn servant || LTP
 remote -u "wps-server/login" so "wps"
+remote -r "wps-server/crowdinDB" os "cdb"
 
 rnt offSetValue = 12
 
@@ -10,7 +11,10 @@ LTP.initiate()
 
 rnt.root(
   def >> do
-    // ---- //
+    {
+      wps.userName:: cdb.userName,
+      wps.pwd:: cdb.pwd,
+      wps.authType:: "CRAM-MD5",
+    },
     version: 1.0.3,
 )
-
